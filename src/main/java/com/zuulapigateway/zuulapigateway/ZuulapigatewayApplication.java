95f36df43c725be57a25152c11e9ffff74a45de9
koderz@123 <sesharatnam889@gmail.com>
2020-09-22 19:14:15 +0530

    zuul first commit

---
+++ src/main/java/com/zuulapigateway/zuulapigateway/ZuulapigatewayApplication.java
@@ -0,0 +1,17 @@
+package com.zuulapigateway.zuulapigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@SpringBootApplication
+@EnableEurekaClient
+@EnableZuulProxy
+public class ZuulapigatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZuulapigatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zuulapigateway/zuulapigateway/ZuulapigatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zuulapigateway.zuulapigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZuulapigatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
